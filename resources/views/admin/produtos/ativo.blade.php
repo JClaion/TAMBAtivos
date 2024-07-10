f37c44d1c01903ea73d93e9a901fa78047cafec0
JVBC743 <jvbc@email.com>
2024-07-10 15:41:14 -0400

    Instalado o 'Breeze with Alpine' e adicionado um layout com os templates blade. Além de ter criado as pastas 'produtos' e 'layout'

---
+++ resources/views/admin/produtos/ativo.blade.php
@@ -0,0 +1,9 @@
+@extends('admin.layouts.app')
+
+@section('title', 'Lista de ativos')
+
+@section('content')
+
+Listinha cri cri
+
+@endsection
---
+++ resources/views/admin/produtos/relatorio.blade.php
@@ -0,0 +1,9 @@
+@extends('admin.layouts.app')
+
+@section('title', 'Relatório geral')
+
+@section('content')
+
+Relatório cri cri
+
+@endsection
