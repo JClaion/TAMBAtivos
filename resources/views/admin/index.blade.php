7b87172b28096c7d403177dd6319b2838e259d9d
JClaion <joseclaionmartins@gmail.com>
2024-07-10 19:14:59 -0400

    AdminScreem, Components and Template

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,8 @@
+@extends('template.main')
+@section('title', 'AdminScreen')
+
+
+@section('content')
+    @include('components.navbar')
+    @include('components.sidebar')
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/AdminScreen', function (){
+    return view('admin.index');
 });
