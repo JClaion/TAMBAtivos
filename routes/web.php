6678633ef674b2e0cbc9d5f3f37a7369ccb3b183
JVBC743 <jvbc@email.com>
2024-07-12 14:45:25 -0400

    Alterado o botão de Dashboard da tela de login, fazendo com que ao clicar nele, o usuário seja redirecionado para a lista se estiver logado.

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,6 @@ Route::group([
 ], function(){
 
     Route::get('/teste_lista', [UserController::class , 'index'])->name('teste_lista.index');
-    Route::post('/teste_lista', [UserController::class, 'store'])->name('teste_lista.store');
     Route::get('/relatorio', [RelatorioController::class , 'index'])->name('relatorio.index');
     Route::get('/ativo', [AtivoController::class , 'index'])->name('ativo.index');
 
@@ -33,13 +32,14 @@ Route::group([
 
     Route::put('/teste_lista/{user}', [UserController::class, 'update'])->name('teste_lista.update');
     Route::delete('/teste_lista/{id}/destroy', [UserController::class, 'destroy'])->name('teste_lista.destroy');
+    Route::post('/teste_lista', [UserController::class, 'store'])->name('teste_lista.store');
 
 });
 
 
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
+Route::get('/lista', function () {
+    return view('admin.user.teste_lista');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
